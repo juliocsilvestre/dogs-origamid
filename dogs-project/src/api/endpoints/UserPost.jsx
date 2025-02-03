e434ee4c3a4e4d722d28d2c57e3778442378104f
Julio Silvestre <jcss.silvestre@gmail.com>
2025-02-03 00:46:06 -0300

    adjusts for build

--- dogs-project/src/api/endpoints/UserPost.jsx
+++ dogs-project/src/api/endpoints/UserPost.jsx
@@ -3,12 +3,58 @@ const UserPost = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    const user = { username, password, email };
+    fetch('https://dogsapi.origamid.dev/json/api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+    });
+  }
   
   
     return (
-    <div>
-    
-    </div>
+      <>
+    <form onSubmit={handlesubmit}>
+      <div>
+        <input
+      type='text'
+      placeholder='username'
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+       <div>
+        <input
+       type='password'
+       placeholder='password'
+       value={password}
+       onChange={(e) => setPassword(e.target.value)}
+        />
+       </div>
+      <div>
+        <input
+      type='email'
+      placeholder='email'
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+        
+      <button>Enviar</button>
+      </form>
+      </>
   )
 }
 
