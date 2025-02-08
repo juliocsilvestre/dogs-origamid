c7e8f20b8c66b0977cd5ace59995355f710997d3
Julio Silvestre <jcss.silvestre@gmail.com>
2025-02-07 23:28:01 -0300

    create routes and css global

--- dogs-project/src/App.jsx
+++ dogs-project/src/App.jsx
@@ -1,10 +1,28 @@
-import Api from './api/Api';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 function App() {
-
-  return (
-      <div><Api/></div>
-
-  )
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Home />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="login/*" element={<Login />} />
+                    <Route path="conta/*" element={<User />} />
+                    <Route path="foto/:id" element={<Photo />} />
+                    <Route path="foto" element={<Feed />} />
+                    <Route path="perfil" element={<User />} />
+                    <Route path="postar" element={<User />} />
+                    <Route path="404" element={<NotFound />} />
+                    <Route path="*" element={<Navigate to="/404" />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
