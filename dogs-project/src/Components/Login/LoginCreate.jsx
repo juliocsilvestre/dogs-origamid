6984863cb242dae2ee496c4f566e45eb95bd4398
Julio Silvestre <jcss.silvestre@gmail.com>
2025-02-27 09:41:29 -0300

    login-form

--- dogs-project/src/Components/Login/LoginCreate.jsx
+++ dogs-project/src/Components/Login/LoginCreate.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-
-const LoginCreate = () => <div>Rota Create acessada!</div>;
+import { Link } from "react-router-dom";
+const LoginCreate = () => {
+    return (
+        <section>
+            <div>
+                <h2>Cadastre-se</h2>
+                <p>Ainda não possui conta? Cadastre-se no site.</p>
+                <Link to="/login">Cadastro</Link>
+            </div>
+        </section>
+    );
+};
 
 export default LoginCreate;
--- dogs-project/src/Components/Login/LoginForm.jsx
+++ dogs-project/src/Components/Login/LoginForm.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const LoginForm = () => {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            console.log("Response:", response);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log("Data:", data);
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+    };
+
     return (
         <section>
             <div>
-                <h2>Cadastre-se</h2>
+                <h1>Login</h1>
+                <form action="" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        onChange={({ target }) => setUsername(target.value)}
+                        value={username}
+                    />
+                    <input
+                        type="password"
+                        onChange={({ target }) => setPassword(target.value)}
+                        value={password}
+                    />
+                    <button>Entrar</button>
+                </form>
                 <p>Ainda não possui conta? Cadastre-se no site.</p>
                 <Link to="create">Cadastro</Link>
             </div>
